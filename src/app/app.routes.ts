03afc9788787919c3b625a287fb8b10dbaa24b9a
Igor <iholovka@gfi.es>
2018-06-08 09:46:31 +0200

    add recources  & components

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+
+export const ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'search/:textoBuscar', component: BuscadorComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
---
+++ src/app/models/heroe.models.ts
@@ -0,0 +1,8 @@
+export interface Heroe {
+    nombre: string;
+    bio: string;
+    img: string;
+    aparicion: string;
+    casa: string;
+    idx?: number;
+}
